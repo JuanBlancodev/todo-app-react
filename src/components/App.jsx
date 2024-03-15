6f49f8f7c1f0f3b7a99b1082e9c2f095566ae1dd
Juan Blanco <juanblancodev@gmail.com>
2024-03-15 01:50:57 -0700

    Add form container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import TodoContainer from './container/todo/TodoContainer'
+import FormContainer from './container/form/FormContainer'
 
 import '../css/index.css'
 
 const App = () => {
   return <main className='d-flex flex-column align-center jusfity-center'>
     <TodoContainer />
+    <FormContainer />
   </main>
 }
 
