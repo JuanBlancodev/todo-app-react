1c9a804e1e9c5c0932351b24d12144b0b05f0172
Juan Blanco <juanblancodev@gmail.com>
2024-03-18 22:19:00 -0700

    postcss implemented

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import terser from '@rollup/plugin-terser'
+import postcss from '@vituum/vite-plugin-postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), terser()],
+  plugins: [
+    react(), 
+    terser(),
+    postcss()
+  ],
 })
