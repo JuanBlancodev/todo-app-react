05bdc6438a8f41c2998729efae0489f930ece607
Juan Blanco <juanblancodev@gmail.com>
2024-03-18 18:03:39 -0700

    dependence react-header-watermark added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
+import { useEffect, useState } from 'react'
 import classNames from 'classnames'
 import useGlobalContext from '../hooks/useGlobalContext'
 import TodoContainer from './todo/TodoContainer'
 import FormContainer from './form/FormContainer'
 import Modal from './modal/Modal'
 import { ToastContainer } from 'react-toastify'
+import ScreenLoader from './ScreenLoader'
+import HeaderWatermark from 'react-header-watermark'
+
+import { PROJECT_NAME, GITHUB_LINK } from '../config/cfg'
 
 import '../css/index.css'
 import 'react-toastify/dist/ReactToastify.css';
-import ScreenLoader from './ScreenLoader'
-import { useEffect, useState } from 'react'
+import 'react-header-watermark/dist/index.css'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
@@ -19,7 +23,10 @@ const App = () => {
   }, [])
 
   return <>
-    { loading && <ScreenLoader /> }
+    { loading ? 
+      <ScreenLoader /> : 
+      <HeaderWatermark projectName={PROJECT_NAME} github={GITHUB_LINK} /> 
+    }
     <main className={classNames('d-flex flex-column align-center jusfity-center', { 'visible': !loading })}>
       <ToastContainer />
       <TodoContainer />
